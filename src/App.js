85f237f36fbf11bc71bb23899265bd63aff804a0
Daniel Ermakov <danielermakov2012@gmail.com>
2024-06-17 01:07:28 +0300

    v1.01 adjusting the setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h1">Roommates Management App</Typography>
+      <Typography variant="body1">
+        Welcome to your roommates management app. Here you can manage all tasks and activities.
+      </Typography>
+    </Container>
   );
 }
 
