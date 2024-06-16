3a05b3e554633e162f2946c1e243921a84240e64
Daniel Ermakov <danielermakov2012@gmail.com>
2024-06-17 01:21:14 +0300

    V1.1 initial setup and template ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
 import './styles/App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
+    <><Header />
     <Container>
       <Typography variant="h1">Roommates Management App</Typography>
       <Typography variant="body1">
         Welcome to your roommates management app. Here you can manage all tasks and activities.
       </Typography>
-    </Container>
+    </Container></>
   );
 }
 
